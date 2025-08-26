a433eb76aaadc11805309d720f62bf9214d435f0
nidhi goyal <ngoel95@gmail.com>
2025-08-26 16:43:21 +0200

    created and commited migrations

---
+++ BatteryDashboard.Server/Migrations/20250826135602_InitialUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BatteryDashboard.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
