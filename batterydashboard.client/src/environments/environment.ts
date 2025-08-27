bb2d88ff48a512beb749cdf4a5deee83541ee2c1
nidhi goyal <ngoel95@gmail.com>
2025-08-27 11:48:22 +0200

    udpate prod api

--- batterydashboard.client/src/environments/environment.ts
+++ batterydashboard.client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authUrl: '/api',
+  authUrl: 'https://iwelldashboard-cpazfpb3emabgbft.westeurope-01.azurewebsites.net/api',
   deviceId: '23.32.003'
 };
